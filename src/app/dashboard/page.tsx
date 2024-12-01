ec2df8b3832b7b2714ec8905fa54eb4e1ad2bb75
Carlos Diaz <shk.cdp@gmail.com>
2024-12-01 22:22:20 +0100

    pruebas tarjetas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,95 @@ type PageProps = {}
 const page = (props: PageProps) => {
   return (
     <ProtectedLayout>
-        <main>
+        <main className={'grid gap-2 md:grid-cols-2 lg:grid-cols-4'}>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Dashboard</CardTitle>
+                    <CardDescription>Dashboard</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Welcome to the dashboard</p>
+
+                </CardContent>
+                
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Dashboard</CardTitle>
+                    <CardDescription>Dashboard</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Welcome to the dashboard</p>
+
+                </CardContent>
+                
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Dashboard</CardTitle>
+                    <CardDescription>Dashboard</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Welcome to the dashboard</p>
+
+                </CardContent>
+                
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Dashboard</CardTitle>
+                    <CardDescription>Dashboard</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Welcome to the dashboard</p>
+
+                </CardContent>
+                
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Dashboard</CardTitle>
+                    <CardDescription>Dashboard</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Welcome to the dashboard</p>
+
+                </CardContent>
+                
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Dashboard</CardTitle>
+                    <CardDescription>Dashboard</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Welcome to the dashboard</p>
+
+                </CardContent>
+                
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Dashboard</CardTitle>
+                    <CardDescription>Dashboard</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Welcome to the dashboard</p>
+
+                </CardContent>
+                
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Dashboard</CardTitle>
+                    <CardDescription>Dashboard</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Welcome to the dashboard</p>
+
+                </CardContent>
+                
+            </Card>
             <Card>
                 <CardHeader>
                     <CardTitle>Dashboard</CardTitle>
